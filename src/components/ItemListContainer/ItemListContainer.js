f62f876ceec22c6b7c2832d5a6a23ab50c486016
Alan Aguilar <93650566+tiltedveggie@users.noreply.github.com>
2022-01-26 20:45:37 -0300

    Update ItemListContainer.js

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,5 @@
 import './ItemListContainer.css'
 
-// function Greetings({ greetings }) {
-//   return <h3>{greetings}</h3>;
-// }
-
 function ItemListContainer({ greetings }) {
   return (
     <section>
